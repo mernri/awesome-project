3be29c53a9823d4abde31d01c3617dd6e701c947
Rita Mernissi <mernissi.rita@gmail.com>
2018-12-11 20:41:08 +0100

    books button, books list, book view

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 
-/* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
-});
+/* we are requiring our collection model */
+const Book = require("../models/book.js");
 
 module.exports = router;
+
+
+/* GET home page */
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+/* creating a books route to list all the books */
+router.get('/books', (req, res, next) => {
+  Book.find()
+    .then(books => {
+      res.render("books", { books });
+    })
+    .catch(error => {
+      console.log(error)
+    })
+});
+
+/* creating a route to see a specific book */ 
+router.get('/book/:id', (req, res, next) => {
+  let bookId = req.params.id;
+  Book.findOne({'_id': bookId})
+    .then(book => {
+      res.render("book-detail", { book })
+    })
+    .catch(error => {
+      console.log(error)
+    })
+});
